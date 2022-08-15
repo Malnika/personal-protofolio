d91717195c3da7339650fe56c21bc194b72afa9e
Malnika <111262502+Malnika@users.noreply.github.com>
2022-08-15 09:29:30 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function fn(){
+
+
+let hiring = document.getElementById("hiring")
+if(hiring.checked==true){
+    document.getElementById("rate").style.display = "block"
+}
+    else{
+        
+        document.getElementById("rate").style.display = "none"
+    }
+}
